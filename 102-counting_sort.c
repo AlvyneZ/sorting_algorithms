d2a0bfd21beb64a803e54cf10e8ad49d1d300e83
AlvyneZ <alvynew@gmail.com>
2023-04-21 19:01:56 +0300

    Counting sort algorithm

---
+++ 102-counting_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+#define PRINT_COUNT_ARRAY	1
+
+/**
+ * counting_sort - sorts an array of non-negative integers in ascending
+ *  order using the Coounting sort algorithm
+ * @array: The array to be sorted
+ * @size: The length of the array
+ *
+ * Return: None
+ */
+void counting_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int *cnt_arr, max;
+
+	if (size == 0)
+	{
+		print_array(NULL, 0);
+		return;
+	}
+	max = array[0];
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	cnt_arr = malloc((++max) * sizeof(int));
+	if (cnt_arr == NULL)
+	{
+		print_array(NULL, 0);
+		return;
+	}
+	for (i = 0; i < (size_t)max; i++)
+		cnt_arr[i] = 0;
+	for (i = 0; i < size; i++)
+		cnt_arr[array[i]]++;
+	for (i = 1; i < (size_t)max; i++)
+		cnt_arr[i] += cnt_arr[i - 1];
+	print_array(cnt_arr, max);
+	for (i = 0, j = 0; i < (size_t)max; i++)
+		for (; j < (size_t)cnt_arr[i]; j++)
+			array[j] = i;
+}
