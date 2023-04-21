03b2da0b63fec261e9f16f920ccb5b162646fe7d
AlvyneZ <alvynew@gmail.com>
2023-04-21 14:43:42 +0300

    Bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+#define PRINT_AFTER_SWAP	1
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order using the
+ *  Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The length of the array
+ *
+ * Return: None
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	int swapped = 1, temp;
+
+	if (size <= 1)
+		return;
+	size--;
+	while (swapped)
+	{
+		swapped = 0;
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swapped = 1;
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				#if PRINT_AFTER_SWAP
+				print_array(array, size);
+				#endif
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # sorting-algorithms
 ## Author: AlvyneZ
-This repository contains C program source code for implementing different sorting algorithms in C
+This repository contains C program source code for implementing different sorting algorithms in C  
+
+### Big-O files
+The 0-O, 1-O, ... files give the time complexities of the implemented sorting algorithms
+ in big-O notation with the following format:
+- O(1)
+- O(n)
+- O(n!)
+- n square -> O(n^2)
+- log(n) -> O(log(n))
+- n * log(n) -> O(nlog(n))
+- n + k -> O(n+k)
+- ...
+And with the following order:
+1. in the best case
+2. in the average case
+3. in the worst case
