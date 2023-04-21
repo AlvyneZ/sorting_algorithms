6d6cf18878599fc47fd1156731414060f2be6de9
AlvyneZ <alvynew@gmail.com>
2023-04-21 14:48:09 +0300

    Fixing short print bug

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -30,7 +30,7 @@ void bubble_sort(int *array, size_t size)
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
 				#if PRINT_AFTER_SWAP
-				print_array(array, size);
+				print_array(array, size + 1);
 				#endif
 			}
 		}
