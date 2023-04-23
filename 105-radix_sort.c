5ab91cc2298ae00117bc093f5c15aeec04e47afe
AlvyneZ <alvynew@gmail.com>
2023-04-23 22:26:31 +0300

    Radix sort

---
+++ 105-radix_sort.c
@@ -0,0 +1,62 @@
+#include "sort.h"
+
+#define PRINT_AFT_SIG_CHNG	1
+
+/**
+ * radix_sort_rec - sorts an array of non-negative integers in ascending
+ *  order using the Radix sort algorithm using a specified base given the
+ *  significant figure to start from
+ * @array: The array to be sorted
+ * @size: The length of the array
+ * @place: The significant figure place to start sorting from
+ * @base: The radix base to be used
+ *
+ * Return: None
+ */
+static void radix_sort_rec(int *array, size_t size, int place, int base)
+{
+	size_t i, j;
+	int *tmp_arr, c;
+
+	for (i = 0; i < size; i++)
+		if (array[i] >= place)
+			break;
+	if (i >= size)
+		return;
+	tmp_arr = malloc(2 * size * sizeof(int));
+	if (tmp_arr == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		tmp_arr[i] = (array[i] / place) % base;
+		tmp_arr[i + size] = array[i];
+	}
+	for (i = 0, c = 0; c < base; c++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (tmp_arr[j] == c)
+			{
+				array[i++] = tmp_arr[j + size];
+			}
+		}
+	}
+	free(tmp_arr);
+	#if PRINT_AFT_SIG_CHNG
+	print_array(array, size);
+	#endif
+	radix_sort_rec(array, size, (place * base), base);
+}
+
+/**
+ * radix_sort - sorts an array of non-negative integers in ascending
+ *  order using the Radix sort algorithm (base 10)
+ * @array: The array to be sorted
+ * @size: The length of the array
+ *
+ * Return: None
+ */
+void radix_sort(int *array, size_t size)
+{
+	radix_sort_rec(array, size, 1, 10);
+}
